a5fb8b35320b7e5077c317b05a4396137390bd06
M---A---B <mab.abbasi1374@gmail.com>
2018-09-10 11:50:09 +0430

    main page modified by MAB

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
       enable-resize-watcher
       fixed
@@ -26,14 +25,11 @@
     </v-navigation-drawer>
     <v-toolbar
       app
-      :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
       </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
@@ -48,24 +44,15 @@
       <HelloWorld/>
     </v-content>
     <v-navigation-drawer
-      temporary
+      persistant
       :right="right"
       v-model="rightDrawer"
       fixed
       app
     >
       <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
