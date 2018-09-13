aea5b63ebb7aad0fcd8d091236400c1ad28da522
Mahdi Parvan <voldorel@gmail.com>
2018-09-13 15:15:46 +0430

    added vuex initialization

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
-import './plugins/vuetify'
 import App from './App.vue'
+import {store} from './store/store'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+	store,
   render: h => h(App)
 }).$mount('#app')
