c1e784bcdc34d03b7a16c904029563b5b37cfbe0
Mahdi Parvan <voldorel@gmail.com>
2018-09-22 11:53:39 +0330

    :)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import '@babel/polyfill'
 import Vue from 'vue'
-// import './plugins/vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
 import {store} from './store/store'
 import Vuetify from 'vuetify'
-import theme from './theme'
-
-Vue.use(Vuetify, {
-  theme
-})
 
 
 Vue.config.productionTip = false
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,7 +6,7 @@ Vue.use(Vuetify, {
   theme: {
     primary: '#ee44aa',
     secondary: '#424242',
-    accent: '#82B1FF',
+    accent: '#4F9BE7',
     error: '#FF5252',
     info: '#2196F3',
     success: '#4CAF50',
