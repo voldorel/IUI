03e3a5da990b4ff187c969c8e9cd644178d4cc0f
mohammad <mab.abbasi1374@gmail.com>
2018-10-01 12:54:10 +0330

    left nav width is dynamic now

--- src/components/LeftNav/Layers.vue
+++ src/components/LeftNav/Layers.vue
@@ -6,8 +6,8 @@
                 label="Search My Project"
                 id="id"
                 class="mx-2"
-                height="50%"
         >
+
         </v-text-field>
         <Tree></Tree>
 
